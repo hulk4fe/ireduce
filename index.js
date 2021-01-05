fcb10bdbd3f85cd461508b0361deb82d61c3d7f1
dengbenye <dengbenye@jd.com>
2021-01-05 17:00:52 +0800

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+const imgmin = require('imagemin')
+const fs = require('fs')
+const path = require('path')
+const imageminPngquant = require('imagemin-pngquant')
+const imageminJpegtran = require('imagemin-jpegtran')
+
+const base = path.join(process.cwd(), 'src')
+const dest = path.join(process.cwd(), 'dist')
+const inputs = fs.readdirSync(base).reduce((pre, cur) => {
+  const file = path.join(base, cur)
+  return fs.statSync(file).isDirectory() ? [...pre] : [...pre, file]
+}, [])
+
+imgmin(inputs, {glob: false, destination: dest, plugins: [
+  imageminPngquant({
+    quality: [0.6, 0.8]
+  }),
+  imageminJpegtran({
+    progressive: true
+  })
+]})
